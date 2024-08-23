7af713aca17b3d303593247b5ac21c98bec87b25
zrdominguez <zechariahd@gmail.com>
2024-08-23 12:09:03 -0500

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers,
+  applyMiddleware, compose
+} from 'redux';
+
+const rootReducer = combineReducers({
+
+});
+
+let enhancer;
+
+if (import.meta.env.MODE !== "production") {
+  const logger = (await import("redux-logger")).default;
+  const composeEnhancers =
+    typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true })
+      : compose;
+  enhancer = composeEnhancers(applyMiddleware(logger));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
