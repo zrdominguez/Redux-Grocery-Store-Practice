1a929ae531f135484bf1eca3129e1e82293e284b
zrdominguez <zechariahd@gmail.com>
2024-08-23 17:18:54 -0500

    clicking purchase button empties the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import CartItem from './CartItem';
 import './Cart.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeFromCart } from '../../store/cart';
 
 function Cart() {
   const cart = useSelector(state => state.cart);
   const produce = useSelector(state => state.produce);
+  const dispatch = useDispatch();
 
   const cartItems = Object.values(cart)
     .map(item => {
@@ -26,6 +28,7 @@ function Cart() {
       "Purchased the following:\n" +
       `${cartItems.map(item => `${item.count} of ${item.name}`).join('\n')}`
     );
+    cartItems.forEach(item => dispatch(removeFromCart(item.id)))
   }
 
   return (
